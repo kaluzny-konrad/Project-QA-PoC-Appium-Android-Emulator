568f90985cecb424c87663299eb577d233731d85
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-06-08 15:47:48 +0200

    add emulator and test

---
+++ test.js
@@ -0,0 +1,30 @@
+const {remote} = require('webdriverio');
+
+const capabilities = {
+  platformName: 'Android',
+  'appium:automationName': 'UiAutomator2',
+  'appium:deviceName': 'first_avd',
+  'appium:appPackage': 'com.android.settings',
+  'appium:appActivity': '.Settings',
+};
+
+const wdOpts = {
+  host: process.env.APPIUM_HOST || 'localhost',
+  port: parseInt(process.env.APPIUM_PORT, 10) || 4444 || 4723,
+  path: '/wd/hub',
+  logLevel: 'info',
+  capabilities,
+};
+
+async function runTest() {
+  const driver = await remote(wdOpts);
+  try {
+    const batteryItem = await driver.$('//*[@text="Battery"]');
+    await batteryItem.click();
+  } finally {
+    await driver.pause(1000);
+    await driver.deleteSession();
+  }
+}
+
+runTest().catch(console.error);
